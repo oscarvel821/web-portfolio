2a8e964d0df2ddec101bd6d2a737f661d7eeabc4
Oscar Velazquez <oscarvelazquez@oscars-mbp-2.mynetworksettings.com>
2024-01-12 05:28:31 -0500

    sub &apos;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ const notFound = () => {
   return (
     <main className="p-4 grow">
       <section className="flex justify-center">
-        <h1 className="text-2xl font-RobotoFlex">Sorry This Page Doesn't Exist</h1>
+        <h1 className="text-2xl font-RobotoFlex">Sorry This Page Doesn&apos;t Exist</h1>
       </section>
     </main>
   )
