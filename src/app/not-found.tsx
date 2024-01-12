e851bb92bf327d5786635ca6994066befdba24ce
Oscar Velazquez <oscarvelazquez@oscars-mbp-2.mynetworksettings.com>
2024-01-12 03:45:35 -0500

    added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const notFound = () => {
+  return (
+    <main className="p-4 grow">
+      <section className="flex justify-center">
+        <h1 className="text-2xl font-RobotoFlex">Sorry This Page Doesn't Exist</h1>
+      </section>
+    </main>
+  )
+}
+
+export default notFound;
