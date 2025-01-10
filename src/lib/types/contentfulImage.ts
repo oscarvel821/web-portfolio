2086ea5182b7fc5fd9807507836914054ed9ac44
Oscar Velazquez <oscarvelazquez@oscars-mbp-2.mynetworksettings.com>
2025-01-09 19:42:52 -0500

    new type for contentful images

---
+++ src/lib/types/contentfulImage.ts
@@ -0,0 +1,8 @@
+//This is our simplifed version of our Asset aka contentful type Image
+export interface ContentfulImage {
+    url : string;
+    alt : string;
+    width : number;
+    height : number;
+}
+
